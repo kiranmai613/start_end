5fbb6794017f8ccc15f5eb450bcfbfa5bf4b14d9
Kiranmai <58254196+kiranmai613@users.noreply.github.com>
2023-03-04 12:53:04 +0530

    Add files via upload
    startwith and endwith

---
+++ start_end.py
@@ -0,0 +1,5 @@
+my_string = "Hello, World!"
+if my_string.startswith("Hello") and my_string.endswith("World!"):
+    print("The string starts with 'Hello' and ends with 'World!'")
+else:
+    print("The string does not start with 'Hello' and/or does not end with 'World!'")
